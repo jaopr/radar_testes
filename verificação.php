9fc59407f946ee14bf58672a6e7553dc5ca60280
João Paulo Rodrigues <128755956+jaopr@users.noreply.github.com>
2024-05-22 19:30:18 -0300

    Removendo coments

--- verificação.php
+++ verificação.php
@@ -1,15 +1,11 @@
 <?php
-// Inicia a sessão
 session_start();
 
-// Verifica se o usuário está logado
 if(isset($_SESSION['username'])) {
-    // Se estiver logado, exibe uma mensagem de verificação
     echo "<h1>Login bem-sucedido!</h1>";
     echo "<p>Você está logado como: " . $_SESSION['username'] . "</p>";
     echo "<p><a href='logout.php'>Logout</a></p>";
 } else {
-    // Se não estiver logado, redireciona para a página de login
     header("Location: telalogin.php");
     exit;
 }
