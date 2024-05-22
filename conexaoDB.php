6e90e9b8c6325772046563a1fb288f44e74b4655
João Paulo Rodrigues <128755956+jaopr@users.noreply.github.com>
2024-05-22 19:26:56 -0300

    Removendo coments

--- conexaoDB.php
+++ conexaoDB.php
@@ -1,19 +1,15 @@
 <?php
-    // Configurações de conexão
     $servidor = "localhost";
     $usuario = "root";
     $senha = "";
     $banco = "radar";
 
-    // Criando a conexão
     $conexao = new mysqli($servidor, $usuario, $senha, $banco);
 
-    // Verificando a conexão
     if ($conexao->connect_error) {
         die("Conexão Falhou: " . $conexao->connect_error);
     }
 
-    // Criando o Banco de Dados
     $sql = "CREATE DATABASE IF NOT EXISTS $banco";
     if ($conexao->query($sql) === TRUE) {
         echo "Banco de dados criado com sucesso! ";
@@ -21,5 +17,4 @@
         echo "Erro ao criar Banco de dados: " . $conexao->error;
     }
 
-    // Não fechamos a conexão aqui
 ?>
